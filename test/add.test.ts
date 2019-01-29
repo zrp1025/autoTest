4a2ebf6d833a9e3e2d9bcd725567b86389365e0b
庄瑞鹏(Johnson) <zrp@cloudpark.cc>
2019-01-29 10:44:42 +0800

    测试

--- test/add.test.ts
+++ test/add.test.ts
@@ -3,7 +3,7 @@ const Main = require("../src/main");
 
 describe("测试addfunction", function() {
     it('1+1', function() {
-        assert.strictEqual(Main.add(1,1), 3);
+        assert.strictEqual(Main.add(1,1), 2);
     })
 })
 
